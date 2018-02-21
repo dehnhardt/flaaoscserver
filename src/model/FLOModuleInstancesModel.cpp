746afcf1bd2563e8b58afa2dcea2e8ee1a5b984c
Hoger Dehnhardt <holger@dehnhardt.org>
2018-02-21 21:37:47 +0100

    moved model to server

---
+++ src/model/FLOModuleInstancesModel.cpp
@@ -0,0 +1,64 @@
+#include "FLOModuleInstancesModel.h"
+#include "../flaaoscsdk/FLOModuleInstanceDAO.h"
+
+#include <QDebug>
+
+FLOModuleInstancesModel::FLOModuleInstancesModel(QObject *parent)
+	: QObject(parent)
+{
+
+}
+
+void FLOModuleInstancesModel::serialize(QXmlStreamWriter *xmlWriter)
+{
+	xmlWriter->writeStartElement("Modules");
+	xmlWriter->writeAttribute("count", QString::number(m_moduleInstancesMap.size() ));
+	for( auto it : m_moduleInstancesMap )
+	{
+		FLOModuleInstanceDAO *i = it;
+		i->serialize(xmlWriter);
+	}
+	xmlWriter->writeEndElement();
+}
+
+void FLOModuleInstancesModel::deserialize(QXmlStreamReader *xmlReader)
+{
+	while(!xmlReader->atEnd())
+	{
+		QXmlStreamReader::TokenType t = xmlReader->readNext();
+		QStringRef s = xmlReader->name();
+		switch( t )
+		{
+			case QXmlStreamReader::TokenType::StartElement:
+				qDebug() << "Model: Element Name: " << s;
+				if( s  == "Module")
+				{
+					FLOModuleInstanceDAO *i = new FLOModuleInstanceDAO();
+					i->deserialize(xmlReader);
+					addFLOModuleInstance(i);
+				}
+				break;
+			case QXmlStreamReader::TokenType::EndElement:
+				if( s == "Modules")
+					return;
+			default:
+				break;
+		}
+	}
+}
+
+void FLOModuleInstancesModel::addFLOModuleInstance(FLOModuleInstanceDAO *moduleInstance)
+{
+	m_moduleInstancesMap[moduleInstance->uuid()] = moduleInstance;
+	emit(moduleAdded(moduleInstance));
+}
+
+FLOModuleInstanceDAO *FLOModuleInstancesModel::getFLOModuleInstance(QUuid uuid)
+{
+	return m_moduleInstancesMap[uuid];
+}
+
+QMap<QUuid, FLOModuleInstanceDAO *> FLOModuleInstancesModel::getModuleInstancesMap()
+{
+	return m_moduleInstancesMap;
+}
---
+++ src/model/FLOModuleInstancesModel.h
@@ -0,0 +1,37 @@
+#ifndef FLOMODULEINSTANCESMODEL_H
+#define FLOMODULEINSTANCESMODEL_H
+
+#include <QObject>
+#include <QMap>
+#include <QUuid>
+#include <QXmlStreamWriter>
+#include <QXmlStreamReader>
+
+class FLOModuleInstanceDAO;
+
+class FLOModuleInstancesModel : public QObject
+{
+	Q_OBJECT
+public:
+	explicit FLOModuleInstancesModel(QObject *parent = nullptr);
+	void serialize(QXmlStreamWriter *xmlWriter);
+	void deserialize(QXmlStreamReader *xmlReader);
+
+public: //setter
+	void addFLOModuleInstance( FLOModuleInstanceDAO *moduleInstance );
+
+public: //getter
+	FLOModuleInstanceDAO *getFLOModuleInstance( QUuid uuid);
+	QMap<QUuid, FLOModuleInstanceDAO *> getModuleInstancesMap();
+
+signals:
+	void moduleAdded( FLOModuleInstanceDAO *moduleInstance);
+	void moduleRemoved( const QUuid &uuid);
+
+public slots:
+
+private:
+	QMap<QUuid, FLOModuleInstanceDAO *> m_moduleInstancesMap;
+};
+
+#endif // FLOMODULEINSTANCESMODEL_H
